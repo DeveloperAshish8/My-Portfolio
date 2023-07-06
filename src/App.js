df126a42df16e6d0600c34cb00e52345e04b3ae4
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2023-07-06 10:20:04 +0530

    preloader added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import Intro from "./components/intro/introduction";
 import About from "./components/about/about";
 import Skills from "./components/Skills/skills";
@@ -8,16 +10,37 @@ import Work from "./components/work/Work";
 import Header from "./components/header/header";
 
 function App() {
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 3000);
+  }, []);
   return (
-    <>
-      <Header />
-      <Intro />
-      <About />
-      <Skills />
-      <Projects />
-      <Work />
-      <Footer />
-    </>
+    <div className="app">
+      {loading ? (
+        <div className="preloader">
+        <ClimbingBoxLoader
+          color={"#496eff"}
+          loading={loading}
+          size={20}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Intro />
+          <About />
+          <Skills />
+          <Projects />
+          <Work />
+          <Footer />
+        </>
+      )}
+    </div>
   );
 }
 
